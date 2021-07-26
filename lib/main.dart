0f3f490a213a3e8e671e2991d6158ecf63817aac
Irfan Maulana <irfan.resimen2018@gmail.com>
2021-07-26 14:37:15 +0700

    kudu jadi

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ import 'package:universal_html/html.dart';
 
 void main() {
   runApp(MyApp());
-  var audioElement = new AudioElement();
-  audioElement.src = "https://e.top4top.io/m_2032whecr1.mp3";
-  document.body!.append(audioElement);
-  audioElement.play();
+  // var audioElement = new AudioElement();
+  // audioElement.src = "https://e.top4top.io/m_2032whecr1.mp3";
+  // document.body!.append(audioElement);
+  // audioElement.play();
 }
 
 class MyApp extends StatelessWidget {
